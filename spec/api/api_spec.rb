b6c95e2e313694e283eaf65594e15e63b9086ee1
Matt Swieboda <mswiebod@gmail.com>
2014-05-12 21:47:08 -0500

    fixed API spec URI's

--- spec/api/api_spec.rb
+++ spec/api/api_spec.rb
@@ -8,18 +8,37 @@ describe Peepster::API do
   end
 
   describe Peepster::API do
+    let(:output) { `ruby app/peepster.rb spec/fixtures/test1.csv spec/fixtures/test2.csv spec/fixtures/test3.csv` }
+    before(:each) { output }
+    after(:each) { File.delete("data/data.csv") }
+
     describe "GET /records/gender" do
-      it "calls records sorted by"
+      it "calls records sorted by" do
+        get "/records/gender"
+
+        expect(last_response.status).to eq 200
+      end
+
       it "returns an array of records sorted by gender (female first), then last name ascending"
     end
 
     describe "GET /records/birthdate" do
-      it "calls records sorted by"
+      it "calls records sorted by" do
+        get "/records/birthdate"
+
+        expect(last_response.status).to eq 200
+      end
+
       it "returns an array of records sorted by birth date, ascending"
     end
 
     describe "GET /records/name" do
-      it "calls records sorted by"
+      it "calls records sorted by" do
+        get "/records/name"
+
+        expect(last_response.status).to eq 200
+      end
+
       it "returns an array of records sorted by last name, descending"
     end
   end
